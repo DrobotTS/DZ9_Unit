36d4f008701c03061a551f7187878bf966db6c62
DrobotTS <90732395+DrobotTS@users.noreply.github.com>
2021-11-26 22:45:42 +0200

    Create dz9_code.py

---
+++ dz9_code.py
@@ -0,0 +1,11 @@
+from class_9HM import UnitGame
+
+data = UnitGame("gamer", "clan")
+data.health = 50
+print(data)
+data.intellect_unit()
+data.treatment_unit()
+data.intellect_unit()
+print(data)
+
+# input("Enter game clan: standart, scientist, soldier: ")
